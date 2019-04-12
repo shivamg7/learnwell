38ad25ee0c053365398e9517ee07f225dffc9675
shivamg7 <playfullittlekid@gmail.com>
2019-04-13 00:03:21 +0530

    1203

--- bsl/models.py
+++ bsl/models.py
@@ -55,3 +55,4 @@ class Attempt(models.Model):
     id = models.AutoField(primary_key=True)
     question = models.ForeignKey(Question,default=None,on_delete=models.CASCADE)
     userAtt = models.ForeignKey(user,on_delete=models.CASCADE)
+    result = models.CharField(max_length=1,choices=(('C','Correct'),('W','Wrong')))
